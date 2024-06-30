e0264dc2da83eac36854a1888d20c38f476c8c69
Alexander Angelidis <alex.m.angelidis@gmail.com>
2024-06-30 23:12:15 +0100

    Framework for bot from discord.py site

--- acc_manager/__main__.py
+++ acc_manager/__main__.py
@@ -1,2 +1,16 @@
+import logging
+
+from acc_manager.discord_main import DiscordBot
+from dotenv import load_dotenv
+
+
+def main() -> None:
+    logging.basicConfig(
+        level=logging.INFO, format="[%(asctime)s] %(levelname)s: %(message)s"
+    )
+    bot = DiscordBot(prefix="!", ext_dir="cogs")
+    bot.run()
+
+
 if __name__ == "__main__":
-    print("Hello, world!")
+    main()
