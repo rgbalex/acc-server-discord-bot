e93ae046846a80057a89ac4f28699d4df92a38c9
Alexander Angelidis <alex.m.angelidis@gmail.com>
2024-07-01 02:31:02 +0100

    simple track selection working

--- cogs/views/event_view.py
+++ cogs/views/event_view.py
@@ -1,22 +1,41 @@
 import discord
 
-class EventJson(discord.ui.Select):
+track_list = [
+    "barcelona",
+    "brands_hatch",
+    "cota",
+    "donington",
+    "hungaroring",
+    "imola",
+    "indianapolis",
+    "kyalami",
+    "laguna_seca",
+    "misano",
+    "monza",
+    "mount_panorama",
+    "nurburgring",
+    "nurburgring_24h",
+    "oulton_park",
+    "paul_ricard",
+    "red_bull_ring",
+    "silverstone",
+    "snetterton",
+    "spa",
+    "suzuka",
+    "valencia",
+    "watkins_glen",
+    "zandvoort",
+    "zolder" 
+]
+
+class TrackSelect(discord.ui.Select):
     def __init__(self):
-        options=[
-            discord.SelectOption(label="Option 1",emoji="👌",description="This is option 1!"),
-            discord.SelectOption(label="Option 2",emoji="✨",description="This is option 2!"),
-            discord.SelectOption(label="Option 3",emoji="🎭",description="This is option 3!")
-            ]
+        options=[ discord.SelectOption(label=f"{track}") for track in track_list ]
         super().__init__(placeholder="Select an option",max_values=1,min_values=1,options=options)
     async def callback(self, interaction: discord.Interaction):
-        if self.values[0] == "Option 1":
-            await interaction.response.edit_message(content="This is the first option from the entire list!")
-        elif self.values[0] == "Option 2":
-            await interaction.response.send_message("This is the second option from the list entire wooo!",ephemeral=False)
-        elif self.values[0] == "Option 3":
-            await interaction.response.send_message("Third One!",ephemeral=True)
+        await interaction.response.send_message(f"You selected {self.values[0]}",ephemeral=True)
 
 class EventJsonView(discord.ui.View):
     def __init__(self, *, timeout = 180):
         super().__init__(timeout=timeout)
-        self.add_item(EventJson())
+        self.add_item(TrackSelect())
